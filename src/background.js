83ac64ddf5fce78e7515d5a54b3d4dd14459ec26
AE Creations <aecreations@users.noreply.github.com>
2026-02-06 17:08:57 -0800

    Minor code cleanup

--- src/background.js
+++ src/background.js
@@ -3,7 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const DISPLAY_NONE = 0;
 const DISPLAY_CONSOLE = 1;
 const DISPLAY_NOTIFCN = 2;
 
@@ -365,7 +364,7 @@ browser.menus.onClicked.addListener((aInfo, aTab) => {
 });
 
 browser.commands.onCommand.addListener(aName => {
-  if (aName === "open-hello-window") {
+  if (aName == "open-hello-window") {
     openGreetingWindow();
   }
 });
